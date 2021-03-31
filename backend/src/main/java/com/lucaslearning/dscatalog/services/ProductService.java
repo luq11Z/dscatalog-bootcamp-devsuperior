e533881680896f5c273b4e4fe05026715bb78540
luq11Z <lucas_segundinho@hotmail.com>
2021-03-31 23:13:03 +0100

    Product repository, service, resource, only GET done

---
+++ backend/src/main/java/com/lucaslearning/dscatalog/services/ProductService.java
@@ -0,0 +1,70 @@
+package com.lucaslearning.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucaslearning.dscatalog.dto.ProductDTO;
+import com.lucaslearning.dscatalog.entities.Product;
+import com.lucaslearning.dscatalog.repositories.ProductRepository;
+import com.lucaslearning.dscatalog.services.exceptions.DatabaseExcpetion;
+import com.lucaslearning.dscatalog.services.exceptions.ObjectNotFoundExcpetion;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Product> list = repository.findAll(pageRequest);
+		return list.map(x -> new ProductDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() -> new ObjectNotFoundExcpetion("Object not found"));
+		return new ProductDTO(entity, entity.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		//entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundExcpetion("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundExcpetion("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseExcpetion("Integrity violation");
+		}
+	}
+
+}
