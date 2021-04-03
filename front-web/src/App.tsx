8274275f13f6fd4d963de4b32f1be739b14ec50f
luq11Z <lucas_segundinho@hotmail.com>
2021-04-04 00:17:20 +0100

    Added Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './core/assets/styles/custom.scss'
+import './App.scss'
+import Navbar from './core/components/Navbar';
 
 const App = () => {
     return (
-        <div className="container mt-5">
-           <h1>App</h1>
-        </div>
+       <Navbar />
     );
 }
 
