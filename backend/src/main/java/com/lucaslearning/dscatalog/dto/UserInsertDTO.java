65920cea4d8c210afac791f1f427103d8775ebae
Lucas Silva <lucas_segundinho@hotmail.com>
2023-03-28 18:13:43 +0100

    Custom ConstraintValidator

--- backend/src/main/java/com/lucaslearning/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/lucaslearning/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.lucaslearning.dscatalog.dto;
 
+import com.lucaslearning.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private static final long serialVersionUID = 1L;
