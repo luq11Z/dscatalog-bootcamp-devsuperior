0657c062c44894096bf56313ed9df73fa938dedd
Lucas Silva <lucas_segundinho@hotmail.com>
2023-05-13 12:53:07 +0100

    Upload endpoint

--- backend/src/main/java/com/lucaslearning/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/lucaslearning/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.lucaslearning.dscatalog.services.exceptions.DatabaseExcpetion;
 import com.lucaslearning.dscatalog.services.exceptions.ObjectNotFoundExcpetion;
 
@@ -20,30 +22,35 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundExcpetion.class)
 	public ResponseEntity<StandardError> objectNotFoundExcpetion(ObjectNotFoundExcpetion e, HttpServletRequest request){
 		StandardError err = new StandardError();
+		
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
 		return ResponseEntity.status(status).body(err);
 	}
 	
 	@ExceptionHandler(DatabaseExcpetion.class)
 	public ResponseEntity<StandardError> databaseException(DatabaseExcpetion e, HttpServletRequest request){
 		StandardError err = new StandardError();
+		
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Database exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
 		return ResponseEntity.status(status).body(err);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
 		ValidationError err = new ValidationError();
+		
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
@@ -54,6 +61,48 @@ public class ResourceExceptionHandler {
 		for (FieldError f : e.getFieldErrors()) {
 			err.addError(f.getField(), f.getDefaultMessage());
 		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonServiceException(AmazonServiceException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("AWS Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClientException(AmazonClientException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("AWS Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<StandardError> illegalArgumentException(IllegalArgumentException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Bad Request");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(err);
 	}
--- backend/src/main/java/com/lucaslearning/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/lucaslearning/dscatalog/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.lucaslearning.dscatalog.services;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +14,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lucaslearning.dscatalog.dto.CategoryDTO;
 import com.lucaslearning.dscatalog.dto.ProductDTO;
+import com.lucaslearning.dscatalog.dto.UriDTO;
 import com.lucaslearning.dscatalog.entities.Category;
 import com.lucaslearning.dscatalog.entities.Product;
 import com.lucaslearning.dscatalog.repositories.CategoryRepository;
@@ -31,6 +34,9 @@ public class ProductService {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
@@ -74,6 +80,11 @@ public class ProductService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseExcpetion("Integrity violation");
 		}
+	}
+	
+	public UriDTO uploadFile(MultipartFile file) {
+		URL url = s3Service.uploadFile(file);
+		return new UriDTO(url.toString());
 	}
 
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
