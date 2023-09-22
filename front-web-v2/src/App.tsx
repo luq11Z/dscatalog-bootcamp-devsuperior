401963e82ee7bfb11f55f7ce1f8029cb5bdd5f80
Lucas Silva <lucas_segundinho@hotmail.com>
2023-09-22 22:24:17 +0100

    Initiating new DSCatalog project.

---
+++ front-web-v2/src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/styles/custom.scss';
+import './app.css';
+import Navbar from 'components/Navbar';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>
+  );
+};
+
+export default App;
